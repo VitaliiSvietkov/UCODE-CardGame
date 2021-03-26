3f43cb8634d11a0a97cab5b3fdeb21616aec42e9
VitaliiSvietkov <a1vitalii.sv@gmail.com>
2021-03-26 10:54:27 +0200

    ...

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
-function rotate() {
-  let cointFace = document.getElementById('coinFace');
-  cointFace.style.transform = 'rotateX(1080deg)';
+function rotateCoin() {
+  let coinFace = document.getElementById('coinFace');
+  coinFace.style.transform = 'rotateX(1080deg)';
 
   let coinBack = document.getElementById('coinBack');
 
   // Create an effect of throwing a coin
-  cointFace.style.width = '130px';
-  cointFace.style.height = '130px';
-  coinBack.style.width = '130px';
-  coinBack.style.height = '130px';
+  coinFace.style.width = '110px';
+  coinFace.style.height = '110px';
+  coinFace.style.right = '10px';
+
+  coinBack.style.width = '110px';
+  coinBack.style.height = '110px';
   setTimeout(() => {
-    cointFace.style.width = '100px';
-    cointFace.style.height = '100px';
-    coinBack.style.width = '100px';
-    coinBack.style.height = '100px';
+    coinFace.style.width = '80px';
+    coinFace.style.height = '80px';
+    coinFace.style.right = '20px';
+    coinBack.style.width = '80px';
+    coinBack.style.height = '80px';
   }, 1000);
 
   // Backup to the initial state without animation
   setTimeout(() => {
-    cointFace.style.transition = 'none';
-    cointFace.style.transform = 'none';
+    coinFace.style.transition = 'none';
+    coinFace.style.transform = 'none';
   }, 2000);
 
   // Reset the animation
   setTimeout(() => {
-    cointFace.style.transition = 'all 2s';
+    coinFace.style.transition = 'all 2s';
   }, 2100);
 }
